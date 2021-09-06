9ceffe9b8567faa813bd6ac32d03af055c85fbf5
Kiss Márton <kissmrton9@gmail.com>
2021-09-06 13:24:21 +0200

    se id-vel se anélkül nem frissül, hanem megdöglik

--- angular/src/app/components/student-form/student-form.component.ts
+++ angular/src/app/components/student-form/student-form.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { StudentHttpService } from 'src/app/service/student-http.service';
+import { Student } from 'src/app/models/student';
+import { switchMap, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
 @Component({
   selector: 'app-student-form',
   templateUrl: './student-form.component.html',
@@ -12,13 +19,41 @@ export class StudentFormComponent implements OnInit {
   reactForm: FormGroup;
 
 
-  constructor() { }
+  student: Student = {
+    _id: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    classroom: {name: ''}
+  }
+
+  student$: Observable<Student> = this.activatedRoute.params.pipe(
+    switchMap(params => this.service.getById(params.id))
+  );
+  
+  id:string = '';
+
+  constructor(
+    private service: StudentHttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.student$.pipe(take(1)).subscribe({
+      next: student => {
+        this.student = student;
+        this.id = student['_id'];
+        delete this.student['_id'];
+        console.log(student);},
+    });
   }
 
   saveStudent(){
-
+    this.service.update(this.student,this.id).pipe(take(1)).subscribe({
+      next: (student) => this.router.navigate(['/','student']),
+      error: (e:any) => console.log(e.message),
+    });
   }
 
 }
