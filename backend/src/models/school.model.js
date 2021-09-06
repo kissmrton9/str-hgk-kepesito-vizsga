f132b6b90ebd746fc2ddb118949f29431bbedc00
Kiss Márton <kissmrton9@gmail.com>
2021-09-06 13:44:52 +0200

    2. feladat

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,44 @@
-/**
+/*
  * Exercise: make School mongoose model
+ export interface School {
+  _id?:string;
+  name:string;
+  city: string;
+  street: string;
+  zipcode: number;
+  classrooms?: string[];  // references for classroom entities
+}
+
  */
+const mongoose = require('mongoose');
+
+const SchoolSchema = mongoose.Schema({
+    name: {
+        type: String,
+        default: '',
+        required: true
+    },
+    city: {
+        type: String,
+        default: '',
+        required: true
+    },
+    street: {
+        type: String,
+        default: '',
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    classrooms: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'classrooms'
+    }]
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('school', SchoolSchema);
